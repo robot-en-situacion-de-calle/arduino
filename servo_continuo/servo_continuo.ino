edd52a7d6a29ee8738b3f4e30dddd34482a74652
Alma Gonzalez <almagonzalez@engineer.com>
2016-08-20 22:47:53 -0500

    Add files via upload

---
+++ servo_continuo/servo_continuo.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+
+#define SERVO_PIN  9
+
+#define CW   1900
+#define STOP 1550
+#define CCW  1000
+
+Servo myServo;
+
+void setup()
+{
+    Serial.begin(57600);
+    myServo.attach(SERVO_PIN);
+    myServo.writeMicroseconds(STOP);
+}
+
+void loop()
+{
+   while (Serial.available() > 0) {
+   int salida = Serial.parseInt();
+   myServo.writeMicroseconds(salida);
+        
+   }
+    
+}
+
+
